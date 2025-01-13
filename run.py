6d0269c79795fa115499d7d4728223686f209a43
Kangxin An <89579419+KennethAnn@users.noreply.github.com>
2025-01-13 11:15:06 +0800

    Update run.py

--- run.py
+++ run.py
@@ -10,8 +10,14 @@
 
 from julia import Main as jl
 from gen_summary import gen_sum
+import os
 
 scenario_name = 'case' # replace by the name of scenario folder
+
+if not os.path.exists(scenario_name + '/outputs'):
+    os.makedirs(scenario_name + '/outputs')
+
+# solve the model and export
 jl.include("core_model.jl")
 jl.run_model(scenario_name) 
 gen_sum(scenario_name)
